acee75a710b711a829b0a85ad6c1c7ce25895214
FAIZA BERRICHI <77019896+FAITH-ORAN@users.noreply.github.com>
2021-06-16 09:39:13 +0200

    Update target1.php

--- target1.php
+++ target1.php
@@ -1,5 +1,5 @@
 <?php
-if (empty($_POST['nom1']) || empty($_POST['prenom1']) )
+if (empty($_POST['nom1']) && empty($_POST['prenom1']) )
 	{echo "ERREUR : tous les champs n'ont pas ete renseignés.";
 	}else{
         echo "Bonjour ".$_POST["prenom1"]." ".$_POST["nom1"];
